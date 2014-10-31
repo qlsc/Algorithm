585bbab60d0eb25e0f05b1342d9881b784ad8421
dll2051 <dll2051@gmail.com>
2014-10-31 12:05:45 +0800

    update hw5

--- hw5/main.c
+++ hw5/main.c
@@ -18,7 +18,7 @@
 #define HIT  1
 #define MISS 0
 
-#define powf(x,y)     ((float)powf((double)(x),(double)(y)))
+#define powf(x,y)     (float)pow((double)(x),(double)(y))
 #define log2f(x)     ((float)log((double)(x)))/(log((double)2))
 
 typedef struct {

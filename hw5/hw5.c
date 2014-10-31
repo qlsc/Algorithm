5549ceb103dc9e1f832bda45bef0c45038854be4
dll2051 <dll2051@gmail.com>
2014-10-31 13:19:19 +0800

    update hw5

--- hw5/hw5.c
+++ hw5/hw5.c
@@ -37,7 +37,7 @@ typedef struct {
 
 Cache createCache(int s, int n, int l)
 {
-	int i,j;
+		int i,j;
     Cache cache = {s,n,l};
     for (i = 0; i < n; i++) {
         int rows = s / ( n * l );
@@ -95,10 +95,10 @@ int visit(Set *set, int tag, int offset)
     return MISS;
 }
 
-void visitAll(Cache *cache, int len, int adrs[])
+void visitAll(Cache *cache, int len, int addrs[])
 {
     printf("Cache: (%i, %i, %i) [\n",cache->s,cache->n,cache->l);
-    
+    printf("addr  tag  index word_offset hit/miss\n");
     // compute number of rows per set
     int rows = (cache->s / cache->l) / cache->n;
     
@@ -106,22 +106,22 @@ void visitAll(Cache *cache, int len, int adrs[])
     int OFFSET_BITS = log2f(cache->l / 4);
     int INDEX_BITS = log2f(rows);
     
-    // calculate max size for offset and idnext
+    // calculate max size for offset and index
     int OFFSET_SIZE = powf(2, OFFSET_BITS) - 1;
     int INDEX_SIZE = powf(2, INDEX_BITS) - 1;
     
     // loop through addresses and visit each
-	int i,j;
+		int i,j;
     for (i = 0; i < len; i++) {
         
         // get address
-        int adr = adrs[i];
+        int addr = addrs[i];
         
         // compute tag, index, offset
         int offset, index, tag;
         offset = index = tag = 0;
         
-        for (j = 0; j < adr; j++) {
+        for (j = 0; j < addr; j++) {
             offset++;
             if (offset > OFFSET_SIZE) {
                 offset = 0;
@@ -140,8 +140,8 @@ void visitAll(Cache *cache, int len, int adrs[])
         int hit = visit(set, tag, offset);
         
         // log to console
-//        printf("%d %d %d\n", tag, index, offset);
-        printf("(%i : %s)\n", adrs[i], hit ? "HIT" : "MISS");
+	     printf("%d %d %d\n", tag, index, offset);
+        printf("(%i : %s)\n", addrs[i], hit ? "hit" : "miss");
     }
     
     printf("]\n\n");
@@ -152,7 +152,7 @@ int main(int argc, char *argv[])
     printf("homework5\n\n");
     
     static int len = 39;
-    static int adrs[] = {
+    static int addrs[] = {
         0,  4,  8,  12, 16, 20, 24, 28,
         32, 36, 40, 44, 48, 52, 56, 60,
         64, 68, 72, 76, 80, 0,  4,  8,
@@ -160,19 +160,19 @@ int main(int argc, char *argv[])
         71, 15, 39, 11, 51, 57, 41
     };
     
-  /*  Cache ex1 = createCache(128, 16, 8);
-    Cache ex2 = createCache(64,  2, 8);
-    Cache ex3 = createCache(128, 8, 16);
-    Cache ex4 = createCache(64,  1, 8);
+  /*  Cache cache1 = createCache(128, 16, 8);
+    Cache cache2 = createCache(64,  2, 8);
+    Cache cache3 = createCache(128, 8, 16);
+    Cache cache4 = createCache(64,  1, 8);
     
-    visitAll(&ex1, len, adrs);
-    visitAll(&ex2, len, adrs);
-    visitAll(&ex3, len, adrs);
-    visitAll(&ex4, len, adrs);*/
+    visitAll(&cache1, len, addrs);
+    visitAll(&cache2, len, addrs);
+    visitAll(&cache3, len, addrs);
+    visitAll(&cache4, len, addrs);*/
     
-    static int tadrs[] = {1, 4, 8, 5, 20, 17, 19, 56, 9, 11, 4, 43, 5, 6, 9, 17};
-    Cache t = createCache(64, 2, 4);
-    visitAll(&t, 16, tadrs);
+    static int addrs[] = {1, 4, 8, 5, 20, 17, 19, 56, 9, 11, 4, 43, 5, 6, 9, 17};
+    Cache cache = createCache(64, 2, 4);
+    visitAll(&cache, 16, addrs);
     
     return 0;
 }
